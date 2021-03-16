e4f2055639c7f0532b6b59d079e5101910948f86
kmz <maartsee@gmail.com>
2021-03-16 18:00:36 +0100

    1.

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,4 +1,26 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DigitsCounter {
+    /*
+    A DigitsCounter osztálynak legyen egy int getCountOfDigits(String s) metódusa,
+    mely megszámolja, hogy hány különböző számjegy van a bemeneti Stringben.
+     */
+
+    public int getCountOfDigits(String s){
+
+        Set<Character> digits= new HashSet<>();
+        if(s!= null){
+            for(Character ch: s.toCharArray()){
+                if(Character.isDigit(ch)){
+                    digits.add(ch);
+                }
+            }
+
+        }
+
+        return digits.size();
+    }
 }
